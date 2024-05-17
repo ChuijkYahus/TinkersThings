17e430c6c0401e5f4d9bdb4afe3230d6d5194b92
KnightMiner <knightminer4@gmail.com>
2024-05-17 07:03:05 -0400

    Move generated resources to a generated folder
    Required reworking the build script to merge the two directories before making the jar, should see if there is a better way than xcopy
    As part of this, fixed generator not generating bindings properly, and removed stale generated files (notably chain and the old embellishments)

--- release.bat
+++ release.bat
@@ -1,9 +1,17 @@
 @echo off
 
+IF EXIST build RMDIR /q /s build
 IF EXIST "Tinker-Things-#.#.#.jar" DEL "Tinker-Things-#.#.#.jar"
+MKDIR build
+
+REM Copy required files into build directory
+XCOPY src build /s /i /q
+XCOPY generated\assets build\assets /s /i /q
 
 REM Zipping contents
-cd src
+cd build
 jar --create --file ../Tinker-Things-#.#.#.jar *
+cd ..
 
 REM Removing build directory
+RMDIR /q /s build
